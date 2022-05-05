ab07dbeaf6f1baae0cfbbaefed3a8a96d199acef
abrojodigital <abrojodigital@gmail.com>
2022-05-04 21:18:15 -0300

    aun a medias el horoscopo

--- src/Components/Horoscopo.js
+++ src/Components/Horoscopo.js
@@ -1,14 +1,37 @@
-import React, {useState, useEffect} from "react";
-
-
+import React, { useState, useEffect } from "react";
 
 function Horoscopo(params) {
+  const [diaMes, setDiaMes] = useState("");
+  const [signo, setSigno] = useState("");
 
-    return (
-        <div className="container">
-        </div>
-    );
+  const getDiaMes = (e) => {
+    setDiaMes(e.target.value);
+  };
 
+  const calcularSigno = (e) => {
+    e.preventDefault();
+    const fecha = new Date(params.fechaNacimiento);
+    const dia = fecha.getDate();
+    const mes = fecha.getMonth();
+    const anio = fecha.getFullYear();
+    const signos = fetch(
+      "https://abrojodigital.github.io/luna-magica/src/Data/horoscopo.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const signos = data.signos;
+        signos.map((signo) => {
+          if (signo.mes === mes && signo.dia === dia) {
+            setSigno(signo.signo);
+          }
+        });
+      });
+  };
+  useEffect(() => {
+    calcularSigno();
+  }, [params.fechaNacimiento]);
+
+  return <div className="container"></div>;
 }
 
 export default Horoscopo;
