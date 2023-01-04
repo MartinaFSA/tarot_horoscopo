9dd32382b78592db809ce0a6eafdb8ef40fe444a
Martina Fernandez <martinafsa8@gmail.com>
2023-01-04 19:35:22 -0300

    build to deploy to Github Pages

--- src/Components/btn-showhoroscopo.js
+++
@@ -1,9 +0,0 @@
-export default function BtnShowhoroscopo(props) {
-    return (
-        <div>
-            <button type="button" className="btn btn-primary" onClick={props.mostrarHoroscopo}>
-                Generar la lectura
-            </button>
-        </div>
-    )
-}
